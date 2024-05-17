bfd4138fd550178cdc2f52e097cba806ebec3acc
funzter <funzter@mail.timesnake.de>
2024-05-17 21:41:54 +0200

    minor

--- src/main/java/de/timesnake/basic/lobby/chat/Plugin.java
+++ src/main/java/de/timesnake/basic/lobby/chat/Plugin.java
@@ -4,17 +4,11 @@
 
 package de.timesnake.basic.lobby.chat;
 
-import de.timesnake.library.basic.util.LogHelper;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-  public static final Plugin LOBBY = new Plugin("Lobby", "BLY",
-      LogHelper.getLogger("Lobby", Level.INFO));
+  public static final Plugin LOBBY = new Plugin("Lobby", "BLY");
 
-  protected Plugin(String name, String code, Logger logger) {
+  protected Plugin(String name, String code) {
     super(name, code);
   }
 }
